e64803ca8c127971b8ac292c8c80e004caafb77a
davalcato <davalcato@yahoo.com>
2023-07-06 14:32:48 -0700

    first commit

---
+++ EcommStoreApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EcommStoreApp
+//
+//  Created by Daval Cato on 7/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EcommStoreApp/EcommStoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommStoreAppApp.swift
+//  EcommStoreApp
+//
+//  Created by Daval Cato on 7/6/23.
+//
+
+import SwiftUI
+
+@main
+struct EcommStoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
