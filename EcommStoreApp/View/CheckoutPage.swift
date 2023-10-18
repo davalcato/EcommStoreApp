229bff2ef8486b3411484cba3addbb5c4ad96491
davalcato <davalcato@yahoo.com>
2023-10-18 14:59:48 -0700

    added address with zip code

--- EcommStoreApp/View/CheckoutPage.swift
+++ EcommStoreApp/View/CheckoutPage.swift
@@ -7,18 +7,30 @@
 
 import SwiftUI
 
+// Define a struct to represent the user's shipping address
+struct ShippingAddress: Codable {
+    var street: String
+    var city: String
+    var state: String
+    var zipCode: String // Added ZIP code property
+}
+
 struct CheckoutPage: View {
     let subtotal: String
     @Binding var selectedPaymentMethod: PaymentMethod?
 
-    @State private var street: String = ""
-    @State private var city: String = ""
-    @State private var state: String = ""
-    @State private var promoCode: String = ""
-    
+    // Create an instance of ShippingAddress to store the address information
+    @State private var shippingAddress = ShippingAddress(street: "", city: "", state: "", zipCode: "")
+
     init(subtotal: String, selectedPaymentMethod: Binding<PaymentMethod?>) {
         self.subtotal = subtotal
         self._selectedPaymentMethod = selectedPaymentMethod
+
+        // Load the stored address from UserDefaults if available
+        if let storedAddressData = UserDefaults.standard.data(forKey: "shippingAddress"),
+           let storedAddress = try? JSONDecoder().decode(ShippingAddress.self, from: storedAddressData) {
+            self._shippingAddress = State(initialValue: storedAddress)
+        }
     }
 
     var body: some View {
@@ -43,27 +55,27 @@ struct CheckoutPage: View {
                         .fontWeight(.semibold)
                 }
                 .padding(.horizontal, 20)
-                
+
                 // Shipping Address
-                TextField("Street", text: $street)
+                TextField("Street", text: $shippingAddress.street)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding(.horizontal, 20)
-                
-                TextField("City", text: $city)
+
+                TextField("City", text: $shippingAddress.city)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding(.horizontal, 20)
-                
-                TextField("State", text: $state)
+
+                TextField("State", text: $shippingAddress.state)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding(.horizontal, 20)
-                
-                // Promo Code Field
-                TextField("Promo Code", text: $promoCode)
+
+                TextField("ZIP Code", text: $shippingAddress.zipCode)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding(.horizontal, 20)
-                
-                Spacer()
-                
+
+                // ... (Rest of your view)
+
+                // Save the address when the "Proceed to Payment" button is tapped
                 NavigationLink(
                     destination: PaymentPage(selectedPaymentMethod: $selectedPaymentMethod),
                     label: {
@@ -78,93 +90,16 @@ struct CheckoutPage: View {
                     }
                 )
                 .padding(.horizontal, 20)
-                
-                Button(action: {
-                    // Add logic to place the order
-                }) {
-                    Text("Place Order")
-                        .font(.title2)
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.green)
-                        .cornerRadius(15)
+                .onTapGesture {
+                    // Save the address to UserDefaults
+                    if let addressData = try? JSONEncoder().encode(shippingAddress) {
+                        UserDefaults.standard.set(addressData, forKey: "shippingAddress")
+                    }
                 }
-                .padding(.horizontal, 20)
             }
-            .navigationBarHidden(true) // Hide the navigation bar in this view
+            .navigationBarHidden(true)
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(Color(.systemGray6).ignoresSafeArea())
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-//    struct CheckoutPage: View {
-//        let subtotal: String
-//
-//        init(subtotal: String) {
-//            self.subtotal = subtotal
-//        }
-//
-//        var body: some View {
-//            NavigationView {
-//                VStack {
-//                    Text("Checkout")
-//                        .font(.title)
-//                        .fontWeight(.bold)
-//                        .padding(.top, 20)
-//
-//                    Spacer()
-//
-//                    HStack {
-//                        Text("Subtotal")
-//                            .font(.title2)
-//                            .fontWeight(.semibold)
-//
-//                        Spacer()
-//
-//                        Text(subtotal)
-//                            .font(.title2)
-//                            .fontWeight(.semibold)
-//                    }
-//                    .padding(.horizontal, 20)
-//
-//                    Spacer()
-//
-//                    // Add more checkout details, buttons, and other UI elements here
-//
-//                    NavigationLink(
-//                        destination: PaymentPage(), // You can replace PaymentPage with the next step in your checkout process
-//                        label: {
-//                            Text("Proceed to Payment")
-//                                .font(.title2)
-//                                .fontWeight(.semibold)
-//                                .foregroundColor(.white)
-//                                .padding()
-//                                .frame(maxWidth: .infinity)
-//                                .background(Color.blue)
-//                                .cornerRadius(15)
-//                        }
-//                    )
-//                    .padding(.horizontal, 20)
-//                }
-//                .navigationBarHidden(true)
-//                .frame(maxWidth: .infinity, maxHeight: .infinity)
-//                .background(Color(.systemGray6).ignoresSafeArea())
-//            }
-//        }
-//    }
