70f7f37fa50eaee1e7ca5721cc81c332ccb024aa
davalcato <davalcato@yahoo.com>
2023-11-13 18:27:27 -0800

    corrected item price amount

--- EcommStoreApp/View/ProductDetailView.swift
+++ EcommStoreApp/View/ProductDetailView.swift
@@ -9,37 +9,28 @@ import SwiftUI
 
 struct ProductDetailView: View {
     var product: Product
-    
-    //for matched geometry effect
     var animation: Namespace.ID
     
-    //shared data model
     @EnvironmentObject var sharedData: SharedDataModel
-    
     @EnvironmentObject var homeData: HomeViewModel
     
     var body: some View {
         VStack {
-            //title bar and product Image
             VStack {
-                //title bar
                 HStack {
-                    
                     Button {
-                        //closing view
-                        withAnimation(.easeInOut){
+                        withAnimation(.easeInOut) {
                             sharedData.showDetailProduct = false
                         }
-                    }label: {
+                    } label: {
                         Image(systemName: "arrow.left")
                             .font(.title2)
                             .foregroundColor(Color.black.opacity(0.7))
                     }
                     Spacer()
-                    
-                    Button{
+                    Button {
                         addToLiked()
-                    }label: {
+                    } label: {
                         Image("Liked")
                             .renderingMode(.template)
                             .resizable()
@@ -49,10 +40,6 @@ struct ProductDetailView: View {
                     }
                 }
                 .padding()
-                
-                //Product Image
-                //Adding matched geometry effect
-                
                 Image(product.productImage)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
@@ -60,17 +47,12 @@ struct ProductDetailView: View {
                     .padding(.horizontal)
                     .offset(y: -12)
                     .frame(maxHeight: .infinity)
-                
             }
             .frame(height: getRect().height / 2.7)
             .zIndex(1)
             
-            //product details
             ScrollView(.vertical, showsIndicators: false) {
-                
-                //Product data
                 VStack(alignment: .leading, spacing: 15) {
-                    
                     Text(product.title)
                         .font(.system(size: 20).bold())
                     
@@ -86,21 +68,19 @@ struct ProductDetailView: View {
                         .font(.system(size: 15))
                         .foregroundColor(.secondary)
                     
-                    Button{
-                        
-                    }label: {
-                        //since we need image at right
-                        Label{
+                    Button {
+                        // Your action for the button
+                    } label: {
+                        Label {
                             Image(systemName: "arrow.right")
-                        }icon: {
+                        } icon: {
                             Text("Full Description")
-                            
                         }
                         .font(.system(size: 15).bold())
                         .foregroundColor(Color("black"))
                     }
                     
-                    HStack{
+                    HStack {
                         Text("Total")
                             .font(.system(size: 17))
                         
@@ -112,90 +92,73 @@ struct ProductDetailView: View {
                     }
                     .padding(.vertical, 20)
                     
-                    //Add button basket
-                    Button{
+                    Button {
                         addToCart()
-                    }label: {
+                    } label: {
                         Text("\(isAddedToCart() ? "Added" : "Add" ) to Basket")
                             .font(.system(size: 20).bold())
                             .foregroundColor(Color("Color"))
                             .padding(.vertical, 20)
                             .frame(maxWidth: .infinity)
                             .background(
-                            Color("black")
-                                .cornerRadius(15)
-                                .shadow(color: Color.black.opacity(0.06), radius: 5, x: 5, y: 5)
+                                Color("black")
+                                    .cornerRadius(15)
+                                    .shadow(color: Color.black.opacity(0.06), radius: 5, x: 5, y: 5)
                             )
                     }
-                    
                 }
                 .padding([.horizontal, .bottom], 20)
                 .padding(.top, 25)
                 .frame(maxWidth: .infinity, alignment: .leading)
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color("Color")
-                            //corner radius for only top side
-    .           clipShape(CustomCorners(corners: [.topLeft,.topRight], radius: 25))                    .ignoresSafeArea()
+            .background(
+                Color("Color")
+                    .clipShape(CustomCorners(corners: [.topLeft, .topRight], radius: 25))
+                    .ignoresSafeArea()
             )
             .zIndex(0)
         }
         .animation(.easeInOut, value: sharedData.likedProducts)
         .animation(.easeInOut, value: sharedData.cartProducts)
         .background(Color.white.ignoresSafeArea())
-        
     }
+    
     func addToLiked() {
-        
-        if let index = sharedData.likedProducts.firstIndex(where: {product in
+        if let index = sharedData.likedProducts.firstIndex(where: { product in
             return self.product.id == product.id
-        }){
-            //remove from liked
+        }) {
             sharedData.likedProducts.remove(at: index)
-        }
-        else{
-            //add to liked
+        } else {
             sharedData.likedProducts.append(product)
         }
     }
     
     func addToCart() {
-        
-        if let index = sharedData.cartProducts.firstIndex(where: {product in
+        if let index = sharedData.cartProducts.firstIndex(where: { product in
             return self.product.id == product.id
-        }){
-            //remove from cart
+        }) {
             sharedData.cartProducts.remove(at: index)
-        }
-        else{
-            //add to cart
+        } else {
             sharedData.cartProducts.append(product)
         }
     }
     
-    //for like turning red color
     func isLiked() -> Bool {
-        
-        return sharedData.likedProducts.contains {product in
-            
+        return sharedData.likedProducts.contains { product in
             return self.product.id == product.id
         }
     }
     
     func isAddedToCart() -> Bool {
-        
-        return sharedData.cartProducts.contains {product in
-            
+        return sharedData.cartProducts.contains { product in
             return self.product.id == product.id
         }
     }
-    
 }
 
 struct ProductDetailView_Previews: PreviewProvider {
     static var previews: some View {
-//        ProductDetailView(product: HomeViewModel().products[0])
-//            .environmentObject(SharedDataModel())
         ContentView()
     }
 }
